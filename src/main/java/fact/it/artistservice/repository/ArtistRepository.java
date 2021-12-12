9c39116ba74a2809d41f1aca8e52af5eea826377
Thijs <r0793167@student.thomasmore.be>
2021-12-12 15:20:10 +0100

    Artist repository

---
+++ src/main/java/fact/it/artistservice/repository/ArtistRepository.java
@@ -0,0 +1,11 @@
+package fact.it.artistservice.repository;
+
+import fact.it.artistservice.model.Artist;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface ArtistRepository extends MongoRepository<Artist, String> {
+    List<Artist> findTimetableByEvent(String eventName);
+    List<Artist> findTimetableByArtist(String artistName);
+}
